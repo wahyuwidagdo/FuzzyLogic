4c0b1a70238a3e32fd0c4241af4bdbf25b503b1c
Wahyu Fajri <wahyufajri31@gmail.com>
2020-01-22 03:24:39 +0700

    Modifikasi front-end

--- resources/views/analisa_fuzzy/create.blade.php
+++ resources/views/analisa_fuzzy/create.blade.php
@@ -9,44 +9,45 @@
 
 <div class="row">
     <div class="col-12">
-        <form action="" method="POST">
+        <form action="/analisa" method="POST">
             <div class="form-group">
-                <label for="fasilitas">Fasilitas Kurang Nyaman</label>
-                <input type="text" name="fasilitas" value="{{ old('fasilitas') }}" class="form-control">
+                <label for="fasilitas_kn">Fasilitas Kurang Nyaman</label>
+                <input type="text" name="fasilitas_kn" value="{{ old('fasilitas') }}" class="form-control">
                 <div>{{ $errors->first('fasilitas') }}</div>
             </div>
 
             <div class="form-group">
-                <label for="fasilitas">Fasilitas Cukup Nyaman</label>
-                <input type="text" name="fasilitas" value="{{ old('fasilitas') }}" class="form-control">
+                <label for="fasilitas_cn">Fasilitas Cukup Nyaman</label>
+                <input type="text" name="fasilitas_cn" value="{{ old('fasilitas') }}" class="form-control">
                 <div>{{ $errors->first('fasilitas') }}</div>
             </div>
 
             <div class="form-group">
-                <label for="fasilitas">Fasilitas Sangat Nyaman</label>
-                <input type="text" name="fasilitas" value="{{ old('fasilitas') }}" class="form-control">
+                <label for="fasilitas_sn">Fasilitas Sangat Nyaman</label>
+                <input type="text" name="fasilitas_sn" value="{{ old('fasilitas') }}" class="form-control">
                 <div>{{ $errors->first('fasilitas') }}</div>
             </div>
 
             <div class="form-group">
-                <label for="pelayanan">Pelayanan Kurang Baik</label>
-                <input type="text" name="pelayanan" value="{{ old('pelayanan') }}" class="form-control">
+                <label for="pelayanan_kb">Pelayanan Kurang Baik</label>
+                <input type="text" name="pelayanan_kb" value="{{ old('pelayanan') }}" class="form-control">
                 <div>{{ $errors->first('pelayanan') }}</div>
             </div>
 
             <div class="form-group">
-                <label for="pelayanan">Pelayanan Cukup Baik</label>
-                <input type="text" name="pelayanan" value="{{ old('pelayanan') }}" class="form-control">
+                <label for="pelayanan_cb">Pelayanan Cukup Baik</label>
+                <input type="text" name="pelayanan_cb" value="{{ old('pelayanan') }}" class="form-control">
                 <div>{{ $errors->first('pelayanan') }}</div>
             </div>
 
             <div class="form-group">
-                <label for="pelayanan">Pelayanan Sangat Baik</label>
-                <input type="text" name="pelayanan" value="{{ old('pelayanan') }}" class="form-control">
+                <label for="pelayanan_sb">Pelayanan Sangat Baik</label>
+                <input type="text" name="pelayanan_sb" value="{{ old('pelayanan') }}" class="form-control">
                 <div>{{ $errors->first('pelayanan') }}</div>
             </div>
 
-            <button type="submit" class="btn btn-primary">Analisa Data</button>
+            <button type="submit" class="btn btn-primary">Input Data</button>
+            @csrf
         </form>
     </div>
 </div>
--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,7 +1,7 @@
-<nav class="navbar navbar-expand-md navbar-dark shadow-sm bg-dark">
+<nav class="navbar navbar-expand-md navbar-light shadow-sm bg-light">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
-            Fuzzy Logic
+            <img src="img/logo.png" alt="logo" width="50">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
